find-xposed-magisk/lobe-chat

💄 style(agent): polish skill suggestion modal UX (#16027)

packages/locales/src/default/chat.ts
@@ -326,24 +326,26 @@ export default {
   'minimap.previousMessage': 'Previous message',
   'minimap.senderAssistant': 'Agent',
   'minimap.senderUser': 'You',
-  'createModal.createBlank': 'Create Blank',
-  'createModal.groupPlaceholder': 'Describe what this group should do...',
-  'createModal.groupTitle': 'What should your group do?',
-  'createModal.placeholder': 'Describe what your agent should do...',
+  'createModal.createBlank': 'Start Blank',
+  'createModal.groupPlaceholder': 'Describe what this Group should do...',
+  'createModal.groupTitle': 'What should this Group do?',
+  'createModal.placeholder': 'Describe what this Agent should do...',
   'createModal.skillSuggestion.actions.createAnyway': 'Create Agent Anyway',
   'createModal.skillSuggestion.actions.createAnywayHint': 'Skill not a fit?',
   'createModal.skillSuggestion.actions.install': 'Add Skill',
-  'createModal.skillSuggestion.actions.openSkills': 'Open Skills',
-  'createModal.skillSuggestion.actions.tryInLobeAI': 'Try in LobeAI',
+  'createModal.skillSuggestion.actions.installing': 'Adding…',
+  'createModal.skillSuggestion.actions.openSkills': 'View in Skills',
+  'createModal.skillSuggestion.actions.tryInLobeAI': 'Use in LobeAI',
   'createModal.skillSuggestion.description':
-    'Even though this is phrased as a role, the reusable part is the workflow. Install it once, then use it across Agents.',
+    'This looks like a reusable workflow. Install the Skill once, then use it across Agents.',
   'createModal.skillSuggestion.installed.description':
-    'You can now use this Skill in LobeAI or add it to any Agent.',
+    'You can use this Skill in LobeAI or add it to any Agent.',
+  'createModal.skillSuggestion.installed.ready': 'Ready in LobeAI',
   'createModal.skillSuggestion.installed.title': 'Skill added',
   'createModal.skillSuggestion.installError':
     "Skill wasn't added. Retry, or create an Agent anyway.",
-  'createModal.skillSuggestion.title': 'This can use a Skill',
-  'createModal.title': 'What should your agent do?',
+  'createModal.skillSuggestion.title': 'A Skill may fit better',
+  'createModal.title': 'What should this Agent do?',
   'claudeCodeInstallGuide.actions.openDocs': 'Open Install Guide',
   'claudeCodeInstallGuide.actions.openSystemTools': 'Open System Tools',
   'claudeCodeInstallGuide.afterInstall':

SmallAi-API/smaihub

packages/locales/src/default/chat.ts
@@ -326,24 +326,26 @@ export default {
   'minimap.previousMessage': 'Previous message',
   'minimap.senderAssistant': 'Agent',
   'minimap.senderUser': 'You',
-  'createModal.createBlank': 'Create Blank',
-  'createModal.groupPlaceholder': 'Describe what this group should do...',
-  'createModal.groupTitle': 'What should your group do?',
-  'createModal.placeholder': 'Describe what your agent should do...',
+  'createModal.createBlank': 'Start Blank',
+  'createModal.groupPlaceholder': 'Describe what this Group should do...',
+  'createModal.groupTitle': 'What should this Group do?',
+  'createModal.placeholder': 'Describe what this Agent should do...',
   'createModal.skillSuggestion.actions.createAnyway': 'Create Agent Anyway',
   'createModal.skillSuggestion.actions.createAnywayHint': 'Skill not a fit?',
   'createModal.skillSuggestion.actions.install': 'Add Skill',
-  'createModal.skillSuggestion.actions.openSkills': 'Open Skills',
-  'createModal.skillSuggestion.actions.tryInLobeAI': 'Try in smai',
+  'createModal.skillSuggestion.actions.installing': 'Adding…',
+  'createModal.skillSuggestion.actions.openSkills': 'View in Skills',
+  'createModal.skillSuggestion.actions.tryInLobeAI': 'Use in smai',
   'createModal.skillSuggestion.description':
-    'Even though this is phrased as a role, the reusable part is the workflow. Install it once, then use it across Agents.',
+    'This looks like a reusable workflow. Install the Skill once, then use it across Agents.',
   'createModal.skillSuggestion.installed.description':
-    'You can now use this Skill in smai or add it to any Agent.',
+    'You can use this Skill in smai or add it to any Agent.',
+  'createModal.skillSuggestion.installed.ready': 'Ready in smai',
   'createModal.skillSuggestion.installed.title': 'Skill added',
   'createModal.skillSuggestion.installError':
     "Skill wasn't added. Retry, or create an Agent anyway.",
-  'createModal.skillSuggestion.title': 'This can use a Skill',
-  'createModal.title': 'What should your agent do?',
+  'createModal.skillSuggestion.title': 'A Skill may fit better',
+  'createModal.title': 'What should this Agent do?',
   'claudeCodeInstallGuide.actions.openDocs': 'Open Install Guide',
   'claudeCodeInstallGuide.actions.openSystemTools': 'Open System Tools',
   'claudeCodeInstallGuide.afterInstall':