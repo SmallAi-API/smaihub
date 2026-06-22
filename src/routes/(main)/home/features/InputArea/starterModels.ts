find-xposed-magisk/lobe-chat

♻️ refactor: use server config for business features (#16015)

src/routes/(main)/home/features/InputArea/starterModels.ts
@@ -1,14 +1,14 @@
-import { ENABLE_BUSINESS_FEATURES } from '@lobechat/business-const';
-
 import type { HomeNewModelItem } from '@/business/client/hooks/useHomeNewModels';
 
 // Chat
-export const NEW_MINIMAX_MODEL = 'MiniMax-M3';
-export const NEW_MINIMAX_PROVIDER = ENABLE_BUSINESS_FEATURES ? 'lobehub' : 'minimax';
-export const NEW_MINIMAX_MODEL_NAME = 'MiniMax M3';
-export const NEW_CHAT_MODEL = 'claude-opus-4-8';
-export const NEW_CHAT_PROVIDER = ENABLE_BUSINESS_FEATURES ? 'lobehub' : 'anthropic';
-export const NEW_CHAT_MODEL_NAME = 'Claude Opus 4.8';
+export const NEW_GLM_MODEL = 'glm-5.2';
+export const NEW_GLM_MODEL_NAME = 'GLM-5.2';
+export const NEW_KIMI_MODEL = 'kimi-k2.7-code';
+export const NEW_KIMI_MODEL_NAME = 'Kimi K2.7 Code';
+
+export const BUSINESS_CHAT_PROVIDER = 'lobehub';
+export const OSS_GLM_PROVIDER = 'zhipu';
+export const OSS_KIMI_PROVIDER = 'moonshot';
 
 // Image
 export const NEW_IMAGE_MODEL = 'gpt-image-2';
@@ -18,36 +18,17 @@ export const NEW_IMAGE_MODEL_NAME = 'GPT Image 2';
 export const NEW_VIDEO_MODEL = 'dreamina-seedance-2-0-260128';
 export const NEW_VIDEO_MODEL_NAME = 'Seedance 2.0';
 
-const BUSINESS_HOME_NEW_MODELS = [
+export const BUSINESS_HOME_NEW_MODELS = [
   {
-    model: NEW_MINIMAX_MODEL,
-    provider: NEW_MINIMAX_PROVIDER,
-    title: NEW_MINIMAX_MODEL_NAME,
+    model: NEW_GLM_MODEL,
+    provider: BUSINESS_CHAT_PROVIDER,
+    title: NEW_GLM_MODEL_NAME,
     type: 'chat',
   },
   {
-    model: NEW_CHAT_MODEL,
-    provider: NEW_CHAT_PROVIDER,
-    title: NEW_CHAT_MODEL_NAME,
-    type: 'chat',
-  },
-  {
-    model: NEW_IMAGE_MODEL,
-    title: NEW_IMAGE_MODEL_NAME,
-    type: 'image',
-  },
-  {
-    model: NEW_VIDEO_MODEL,
-    title: NEW_VIDEO_MODEL_NAME,
-    type: 'video',
-  },
-] satisfies HomeNewModelItem[];
-
-const OSS_HOME_NEW_MODELS = [
-  {
-    model: NEW_CHAT_MODEL,
-    provider: NEW_CHAT_PROVIDER,
-    title: NEW_CHAT_MODEL_NAME,
+    model: NEW_KIMI_MODEL,
+    provider: BUSINESS_CHAT_PROVIDER,
+    title: NEW_KIMI_MODEL_NAME,
     type: 'chat',
   },
   {
@@ -62,6 +43,27 @@ const OSS_HOME_NEW_MODELS = [
   },
 ] satisfies HomeNewModelItem[];
 
-export const DEFAULT_HOME_NEW_MODELS = ENABLE_BUSINESS_FEATURES
-  ? BUSINESS_HOME_NEW_MODELS
-  : OSS_HOME_NEW_MODELS;
+export const OSS_HOME_NEW_MODELS = [
+  {
+    model: NEW_GLM_MODEL,
+    provider: OSS_GLM_PROVIDER,
+    title: NEW_GLM_MODEL_NAME,
+    type: 'chat',
+  },
+  {
+    model: NEW_KIMI_MODEL,
+    provider: OSS_KIMI_PROVIDER,
+    title: NEW_KIMI_MODEL_NAME,
+    type: 'chat',
+  },
+  {
+    model: NEW_IMAGE_MODEL,
+    title: NEW_IMAGE_MODEL_NAME,
+    type: 'image',
+  },
+  {
+    model: NEW_VIDEO_MODEL,
+    title: NEW_VIDEO_MODEL_NAME,
+    type: 'video',
+  },
+] satisfies HomeNewModelItem[];

SmallAi-API/smaihub

src/routes/(main)/home/features/InputArea/starterModels.ts
@@ -1,12 +1,14 @@
-import { ENABLE_BUSINESS_FEATURES } from '@lobechat/business-const';
-
 import type { HomeNewModelItem } from '@/business/client/hooks/useHomeNewModels';
 
 // Chat
+export const NEW_CLAUDE_MODEL = 'claude-opus-4-8';
+export const NEW_CLAUDE_MODEL_NAME = 'Claude 4.8 Opus';
+export const NEW_GPT_MODEL = 'gpt-5.5';
+export const NEW_GPT_MODEL_NAME = 'GPT 5.5';
 
-export const NEW_CHAT_MODEL = 'claude-opus-4-8';
-export const NEW_CHAT_PROVIDER = 'smai';
-export const NEW_CHAT_MODEL_NAME = 'Claude Opus 4.8';
+export const BUSINESS_CHAT_PROVIDER = 'smai';
+export const OSS_CLAUDE_PROVIDER = 'smai';
+export const OSS_GPT_PROVIDER = 'smai';
 
 // Image
 export const NEW_IMAGE_MODEL = 'gpt-image-2';
@@ -16,11 +18,17 @@ export const NEW_IMAGE_MODEL_NAME = 'GPT Image 2';
 export const NEW_VIDEO_MODEL = 'dreamina-seedance-2-0-260128';
 export const NEW_VIDEO_MODEL_NAME = 'Seedance 2.0';
 
-const BUSINESS_HOME_NEW_MODELS = [
+export const BUSINESS_HOME_NEW_MODELS = [
   {
-    model: NEW_CHAT_MODEL,
-    provider: NEW_CHAT_PROVIDER,
-    title: NEW_CHAT_MODEL_NAME,
+    model: NEW_CLAUDE_MODEL,
+    provider: BUSINESS_CHAT_PROVIDER,
+    title: NEW_CLAUDE_MODEL_NAME,
+    type: 'chat',
+  },
+  {
+    model: NEW_GPT_MODEL,
+    provider: BUSINESS_CHAT_PROVIDER,
+    title: NEW_GPT_MODEL_NAME,
     type: 'chat',
   },
   {
@@ -35,11 +43,17 @@ const BUSINESS_HOME_NEW_MODELS = [
   },
 ] satisfies HomeNewModelItem[];
 
-const OSS_HOME_NEW_MODELS = [
+export const OSS_HOME_NEW_MODELS = [
   {
-    model: NEW_CHAT_MODEL,
-    provider: NEW_CHAT_PROVIDER,
-    title: NEW_CHAT_MODEL_NAME,
+    model: NEW_CLAUDE_MODEL,
+    provider: OSS_CLAUDE_PROVIDER,
+    title: NEW_CLAUDE_MODEL_NAME,
+    type: 'chat',
+  },
+  {
+    model: NEW_GPT_MODEL,
+    provider: OSS_GPT_PROVIDER,
+    title: NEW_GPT_MODEL_NAME,
     type: 'chat',
   },
   {
@@ -53,7 +67,3 @@ const OSS_HOME_NEW_MODELS = [
     type: 'video',
   },
 ] satisfies HomeNewModelItem[];
-
-export const DEFAULT_HOME_NEW_MODELS = ENABLE_BUSINESS_FEATURES
-  ? BUSINESS_HOME_NEW_MODELS
-  : OSS_HOME_NEW_MODELS;