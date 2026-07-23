680d72736e52bea7d64a432c14ce8221f4493a05
Rdmclin2 <rdmclin2@gmail.com>
2026-07-23 23:30:34 +0800

    🐛 fix: resource permission controls, chat-input hints & wechat push (#17468)

--- src/libs/swr/keys.ts
+++ src/libs/swr/keys.ts
@@ -772,6 +772,7 @@ export const messengerKeys = {
     tokenScopeKey,
   ]),
   peek: def('messenger:peek', (randomId: string) => ['messenger:peek', randomId]),
+  pushWindow: def('messenger:pushWindow', (platform: string) => ['messenger:pushWindow', platform]),
 };
 
 // ---- verify (deliverable judging) ---------------------------------------
