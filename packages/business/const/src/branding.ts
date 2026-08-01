f77ae24f5c2005279ea8f162fc040e4acbef6efe
Innei <i@innei.in>
2026-08-02 02:10:00 +0800

    ✨ feat: support Apple Smart App Banner via APPLE_APP_STORE_ID (#17813)
    * ✨ feat: support Apple Smart App Banner via APPLE_APP_STORE_ID
    
    * ✨ feat: inject Smart App Banner meta on auth SPA pages

--- packages/business/const/src/branding.ts
+++ packages/business/const/src/branding.ts
@@ -36,5 +36,7 @@ export const BRANDING_EMAIL = {
 
 export const BRANDING_PROVIDER = 'smai';
 
+export const APPLE_APP_STORE_ID = '';
+
 export const COPYRIGHT = `© ${new Date().getFullYear()} ${ORG_NAME}`;
 export const COPYRIGHT_FULL = `${COPYRIGHT}. All rights reserved.`;
